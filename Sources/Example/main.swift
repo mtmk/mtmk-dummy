64fb0546419e6c5f418034652b45a1b245704fba
Ziya Suzen <ziya@suzen.net>
2024-01-25 18:03:10 +0000

    init

---
+++ Sources/Example/main.swift
@@ -0,0 +1,6 @@
+import Foundation
+import MetemekDummy
+
+let calc = SimpleCalculator()
+let result = calc.add(1, 2)
+print("the result is \(result)")
---
+++ Sources/MetemekDummy/SimpleCalculator.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public class SimpleCalculator {
+    
+    public init() {
+    }
+    
+    // Function to perform addition
+    public func add(_ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
